aa411168cbbbb896dc4bc5411ef18d887a30eb4b
Kiran <kichalla@microsoft.com>
2021-12-21 07:22:50 -0800

    Add project files.

---
+++ SampleAspNetCoreWebApiApp/Controllers/InfoController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleAspNetCoreWebApiApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InfoController : ControllerBase
+    {
+        private readonly ILogger<InfoController> _logger;
+
+        public InfoController(ILogger<InfoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var sb = new StringBuilder();
+            
+            sb.AppendLine($"Host: {Request.Host.Host}");
+            sb.AppendLine($"Port: {Request.Host.Port}");
+            var connectionInfo = Request.HttpContext.Connection;
+            sb.AppendLine($"RemoteIpAddress: {connectionInfo.RemoteIpAddress}");
+            sb.AppendLine($"RemotePort: {connectionInfo.RemotePort}");
+            sb.AppendLine($"LocalIpAddress: {connectionInfo.LocalIpAddress}");
+            sb.AppendLine($"LocalPort: {connectionInfo.LocalPort}");
+            foreach(var header in Request.Headers)
+            {
+                sb.AppendLine($"{header.Key}:{header.Value}");
+            }
+            return Content(sb.ToString());
+        }
+    }
+}
